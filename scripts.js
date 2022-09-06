387fd9be3fc839e59a2e2251f5d08d2aaa171e54
Tamibadger <jochentaminau@gmail.com>
2022-09-06 16:28:04 +0200

    Add playRound function

--- scripts.js
+++ scripts.js
@@ -13,6 +13,54 @@ function getComputerChoice(){
             break;        
 
     }
-    console.log(computerInput);
+    return(computerInput);
 }
-getComputerChoice();
+
+function capitalize(s){
+const capitalizedString= s[0].toUpperCase+ s.slice(1);
+return capitalizedString;
+}
+const computerSelection= getComputerChoice();
+let playerSelection= capitalize(prompt("Please choose Rock,Paper or Scissors"));
+
+
+function playRound(playerSelection,computerSelection){
+    switch(playerSelection){
+        case "Rock":
+            if(computerSelection==="Rock"){
+                return("That's a tie, go again!");
+            }
+            else if(computerSelection==="Paper"){
+                return("You lose! Paper beats rock");
+            }
+            else if (computerSelection==="Scissors"){
+                return("You win! Rock beats scissors");
+            }
+            break;
+        case "Paper":
+            if(computerSelection==="Rock"){
+                return("You win! Paper beats rock");
+            }
+            else if(computerSelection==="Paper"){
+                return("That's a tie, go again!");
+            }
+            else if (computerSelection==="Scissors"){
+                return("You lose! Paper beats rock");
+            }
+            break;
+         case "Scissors":
+            if(computerSelection==="Rock"){
+                return("You lose! Rock beats scissors");
+            }
+            else if(computerSelection==="Paper"){
+                return("You win! Scissors beats paper");
+            }
+            else if (computerSelection==="Scissors"){
+                return("That's a tie, go again");
+            }
+            break;    
+}
+}
+
+console.log(computerSelection);
+console.log(playRound(playerSelection,computerSelection));
